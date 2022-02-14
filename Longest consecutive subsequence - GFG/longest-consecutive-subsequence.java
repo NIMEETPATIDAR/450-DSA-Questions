ba6ebb91a558385d4a6b120d3ac3557cd3deba4c
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-02-14 23:05:54 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,89 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   int result=Integer.MIN_VALUE;
+	   Set<Integer> set=new HashSet<Integer>();
+	   for(int i=0;i<N;i++)
+	   {
+	       set.add(arr[i]);
+	   }
+	   for(int i=0;i<N;i++)
+	   {
+	       if(!set.contains(arr[i]-1))
+	       {
+	           int j=arr[i];
+	           while(set.contains(j))
+	           {
+	               j++;
+	           }
+	           result=Math.max(result,j-arr[i]);
+	       }
+	   }
+	   return result;
+	}
+}
