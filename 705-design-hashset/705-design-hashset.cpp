7a2ad016b2170aaf5176a4241edcec31c4a944f1
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-04-21 15:18:31 +0530

    Time: 201 ms (31.51%), Space: 180 MB (7.92%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+public:
+    //int arr[100000+1];
+    vector<int> arr;
+    MyHashSet() {
+        arr.resize(1e6+1,0);
+        
+    }
+    
+    void add(int key) {
+        arr[key]=1;
+        
+    }
+    
+    void remove(int key) {
+        arr[key]=0;
+    }
+    
+    bool contains(int key) {
+        return arr[key]==1;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
