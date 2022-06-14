d33590adbd49a522f6351bb860a03e8e066df06f
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-06-14 19:44:46 +0530

    Time: 3 ms (57.20%), Space: 8.4 MB (41.69%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,7 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution 
+
+class Solution
+{
+    public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        stack<TreeNode*> st;
+        TreeNode* node=root;
+        vector<int> inorder;
+        
+        while(true)
+        {
+            if(node!=NULL)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            else
+            {
+                if(st.empty()==true) break;
+                node=st.top();
+                st.pop();
+                inorder.push_back(node->val);
+                node=node->right;
+            }
+           
+        }
+         return inorder;
+    }
+};
+
+
+
+
+
+
+
+
+/*class Solution 
 {
 public:
     vector<int> inorderTraversal(TreeNode* root) 
@@ -26,7 +64,7 @@ public:
         recursiveInorder(root->right,res);
     }
 };
-
+*/
 
 
 
