bb0935fef2e73693f0da55af61451f9a866a87dd
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-06-20 12:49:50 +0530

    Time: 28 ms (73.01%), Space: 53.8 MB (70.55%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -2,7 +2,27 @@ class Solution
 {
     public int minimumLengthEncoding(String[] words) 
     {
-        Arrays.sort(words,(a,b)->b.length()-a.length());
+        Set<String> wordSet=new HashSet<>(Arrays.asList(words));
+        for(String s:words)
+        {
+            for(int i=1;i<s.length();i++)
+            {
+                wordSet.remove(s.substring(i));
+            } 
+        }
+        int length=0;
+        for(String s:wordSet)
+        {
+            length+=s.length()+1;
+        }
+        return length;
+    }
+}
+
+/*
+Method 1
+
+Arrays.sort(words,(a,b)->b.length()-a.length());
         StringBuilder encoding=new StringBuilder();
         for(String s:words)
         {
@@ -12,5 +32,4 @@ class Solution
             }
         }
         return encoding.length();
-    }
-}
+*/
