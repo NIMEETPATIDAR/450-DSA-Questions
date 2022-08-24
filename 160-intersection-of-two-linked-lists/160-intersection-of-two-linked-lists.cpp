132a91e57e03524a3d5536d6a55a07b2b7b9d1d2
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-08-24 16:05:04 +0530

    Time: 78 ms (48.02%), Space: 14.3 MB (98.89%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -6,9 +6,11 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
         ListNode *a=headA, *b=headB;
         int count1=0, count2=0;
 		
@@ -17,23 +19,25 @@ public:
         for(ListNode *curr=headB;curr!=NULL; curr=curr->next) count2++;
         
 		//If linked list 1 is longer, we traverse it, till it becomes equal to length of second...
-        while(count1>count2) {
+        while(count1>count2) 
+        {
             count1--;
             a=a->next;
         }
         
 		//If second one is longer, we traverse it, till it becomes equal to length of first...
-        while(count2>count1) {
+        while(count2>count1) 
+        {
             count2--;
             b=b->next;
         }
         
 		//Since length of both is now equal, we traverse them together, and break if the nodes become equal...
-        while(a!=b) {
+        while(a!=b) 
+        {
             a=a->next;
             b=b->next;
         }
-        
         return a;
     }
 };
