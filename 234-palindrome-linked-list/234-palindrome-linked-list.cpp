b1c3ce7a22556521888e7a2208d063a0296bf93e
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-08-25 22:22:41 +0530

    Time: 451 ms (25.73%), Space: 118 MB (59.47%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,19 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
-    ListNode* temp;
-    
-    bool check(ListNode* head) {
-        if (NULL == head) return true;
-        bool res = check(head->next) & (temp->val == head->val);
-        temp = temp->next;
-        return res;
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *pre=NULL;
+        ListNode *next=NULL;
+        while(head!=NULL)
+        {
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
     }
     
-    bool isPalindrome(ListNode* head) {
-        temp = head;
-        return check(head);
+    
+    bool isPalindrome(ListNode* head) 
+    {
+        if(head==NULL || head->next==NULL) return true;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        
+        while(slow!=NULL)
+        {
+            if(head->val!=slow->val) return false;
+            head=head->next;
+            slow=slow->next;
+        }
+        return true;
     }
 };
