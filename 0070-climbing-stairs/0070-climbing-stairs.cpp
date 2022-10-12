5486dfa61c841507a7eb5e4ab593c32eb0b84eca
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-10-13 01:33:20 +0530

    Time: 5 ms (14.27%), Space: 5.7 MB (97.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,3 +1,26 @@
+// By using Space Optimization
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        int a=1;
+        int b=1;
+        int c=1;
+        for(int i=2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
+
+
+
+
+/*
 // By using Dynamic Programming (Tabulation)
 class Solution 
 {
@@ -16,8 +39,7 @@ public:
         return arr[n];
     }
 };
-
-
+*/
 
 /*
 // By using Dynamic Progaramming (Memoization)
