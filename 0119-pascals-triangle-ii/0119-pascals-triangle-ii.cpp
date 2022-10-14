f530cedffa730f015757d1e6cab916d07466e5c7
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-10-14 18:09:57 +0530

    Time: 3 ms (49.49%), Space: 7.2 MB (5.40%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    vector<int> getRow(int rowIndex) 
+    {
+        if(rowIndex==0) return {1};
+        vector<int> currentRow={1};
+        vector<int> previousRow=getRow(rowIndex-1);
+        for(int i=1;i<rowIndex;i++)
+        {
+            currentRow.push_back(previousRow[i-1]+previousRow[i]);
+        }
+        currentRow.push_back(1);
+        return currentRow;
+    }
+};
