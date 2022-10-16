a91f140eef7597baf8e5885b1b0786877bfdd6ff
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-10-16 08:11:43 +0530

    Time: 56 ms (5.03%), Space: 7.2 MB (91.05%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+public:
+    bool isPalindrome(string s) 
+    {
+        int j=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]>=97 && s[i]<=122 || s[i]>=65 && s[i]<=90 || s[i]>=48 && s[i]<=57) 
+            {
+                s[j]=tolower(s[i]);
+                j++;
+            }
+        }
+        for(int i=0;i<j;i++)
+        {
+            cout<<s[i];
+        }
+        int l=0;
+        int k=j-1;
+        while(l<k)
+        {
+            if(s[l]!=s[k]) return false;
+            l++;
+            k--;
+        }
+        return true;
+    }
+};
