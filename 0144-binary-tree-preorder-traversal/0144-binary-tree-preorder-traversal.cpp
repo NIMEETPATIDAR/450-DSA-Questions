e20eae44a6cb5aafb90f21275557534acc90efd7
NIMEETPATIDAR <78745440+NIMEETPATIDAR@users.noreply.github.com>
2022-10-16 13:49:30 +0530

    Time: 4 ms (41.24%), Space: 8.5 MB (41.99%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,6 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+
+class Solution 
+{
+public:
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int> v;
+        if(root==NULL) return v;
+        stack<TreeNode *> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            root=st.top();
+            st.pop();
+            v.push_back(root->val);
+            if(root->right!=NULL) st.push(root->right);
+            if(root->left!=NULL) st.push(root->left);
+        }
+        return v;
+    }
+};
+
+/*
 class Solution 
 {
 public:
@@ -27,3 +52,4 @@ public:
     }
     
 };
+*/
